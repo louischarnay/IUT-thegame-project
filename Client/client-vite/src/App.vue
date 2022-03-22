39796a2e44be430c0c8ea5542947a8886b716832
Louis Charnay <louis.charnay@orange.fr>
2022-03-22 17:04:58 +0100

    creating game interface

--- Client/client-vite/src/App.vue
+++ Client/client-vite/src/App.vue
@@ -1,11 +1,34 @@
 <script setup>
+import Card from "./components/Card.vue";
 
 </script>
 
 <template>
-
+    <div>
+        <div class="stacks">
+            <div id="stack"></div>
+            <div id="stack"></div>
+            <div id="stack"></div>
+            <div id="stack"></div>
+        </div>
+        <div class="cards">
+            <Card value="0"/>
+        </div> 
+    </div>
 </template>
 
 <style>
-
+    .stacks{
+        position: absolute;
+        display: flex;
+        align-items: center;
+        text-align: center;
+    }
+    #stack {
+        width: 200px;
+        height: 300px;
+        border-radius: 10px;
+        background-color: red;
+        margin: 10px;
+    }
 </style>
---
+++ Client/client-vite/src/components/Card.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        props: {
+            value: ""
+        }
+    }
+</script>
+
+<template>
+    <div id="card">
+        <p id="value" >{{this.value}}</p>
+    </div>
+</template>
+
+<style>
+    #card {
+        top: 340px;
+        width: 200px;
+        height: 300px;
+        margin: 10px;
+        border-radius: 10px;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: green;
+    }
+    #value {
+        color: white;
+        font-size: 100px;
+    }
+</style>
