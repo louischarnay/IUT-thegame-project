9c7504bf4c958dd9a25a269e9b87485e93d0be4d
Louis Charnay <louis.charnay@orange.fr>
2022-04-10 12:18:42 +0200

    processus lobby complet

--- ClientAsync/clientAsync.cpp
+++ ClientAsync/clientAsync.cpp
@@ -18,9 +18,51 @@ int main(int argc, char* argv[])
     Player player(0);
 
     char *title = "[ THE GAME ]";
-    char *loading = "Loading...";
-    
+    char str4[2];
+    char str5[2];
+    char str6[2];
+    int cpt = 0;
+    char lobby_mesg1[] = "Créer une partie (c)";
+    char lobby_mesg2[] = "Rejoindre une partie (j)";
+    char lobby_mesg3[] = "Indiquez la lettre correspondante à l'action c/j: ";
+    char lobby_mesg4[] = "Indiquez le nombre max de participants: ";
+    char lobby_mesg5[] = "Indiquez l\'identifiant du salon que vous souhaitez rejoindre: ";
+
+    bool lobby = true;
+
     initscr();
+    while (lobby){
+        clear();
+        cpt = 0;
+        mvprintw(0, (COLS / 2) - (strlen(title) / 2), title);
+        mvprintw(LINES / 4 + cpt, (strlen(lobby_mesg1) / 2) - (strlen(lobby_mesg1) / 2), lobby_mesg1);
+        cpt++;
+        mvprintw(LINES / 4 + cpt, (strlen(lobby_mesg2) / 2) - (strlen(lobby_mesg2) / 2), lobby_mesg2);
+        cpt++;
+        mvprintw(LINES / 4 + cpt, (strlen(lobby_mesg3) / 2) - (strlen(lobby_mesg3) / 2), lobby_mesg3);
+        cpt++;
+        getstr(str4);
+        if (strcmp(str4, "c") == 0){
+            mvprintw(LINES / 4 + cpt, (strlen(lobby_mesg4) / 2) - (strlen(lobby_mesg4) / 2), lobby_mesg4);
+            cpt++;
+            getstr(str5);
+            lobby = false;
+            //crée la room avec str5 participants max
+        }
+        else if (strcmp(str4, "j") == 0){
+            //affichage des rooms
+            mvprintw(LINES / 4 + cpt, (strlen(lobby_mesg5) / 2) - (strlen(lobby_mesg5) / 2), lobby_mesg5);
+            cpt++;
+            getstr(str6);
+            lobby = false;
+            //rejoint la room str6
+        }
+    }
+
+    char *loading = "Loading...";
+
+    //Attente participants
+    
     while(1) {
         clear();
         mvprintw(0, (COLS / 2) - (strlen(title) / 2), title);
@@ -30,6 +72,7 @@ int main(int argc, char* argv[])
             break;
     }
 
+    //Le joueur reçoit les cartes
     player.dealCard(1);
     player.dealCard(2);
     player.dealCard(3);
@@ -39,7 +82,6 @@ int main(int argc, char* argv[])
     strcpy(stacks, stack.c_str());
 
     char cards[300] = "Vos cartes sont: ";
-    int cpt = 0;
     char mesg1[] = "Choisissez une stack: ";
     char mesg12[] = "Merci de jouer une stack valide !";
     char mesg2[] = "Choisissez une carte: ";
@@ -51,6 +93,7 @@ int main(int argc, char* argv[])
     char str1[2];
     char str2[2];
     char str3[250];
+
     strcat(cards, player.getCards().c_str());
 
     bool game = true;
